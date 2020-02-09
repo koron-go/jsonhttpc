29120dd7be46b4a3de41b83a16841cd132a2f72b
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-02-10 00:28:41 +0900

    jsonhttpc.Do() with default client

---
+++ default.go
@@ -0,0 +1,12 @@
+package jsonhttpc
+
+import "context"
+
+// DefaultClient is used for `jsonhttpc.Do()` for convenience.
+var DefaultClient = &Client{}
+
+// Do makes a HTTP request to the server with JSON body, and parse a JSON in
+// response body using `DefaultClient`.
+func Do(ctx context.Context, method, pathOrURL string, body, receiver interface{}) error {
+	return DefaultClient.do(ctx, method, pathOrURL, body, receiver)
+}
